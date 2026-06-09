865043bbaac1d71346da63f0132d0051d408a74c
euclidesmoulin <130880618+euclidesmoulin@users.noreply.github.com>
2026-06-09 23:10:09 +0000

    FOR

---
+++ FOR.cpp
@@ -0,0 +1,18 @@
+  // (iniciliação; condição; incremento)  
+    //{código}
+    //for ( ini;cond/inc)
+    // { cod }
+
+#include <stdio.h>
+
+    int main() {
+  // (iniciliação; condição; incremento)
+        for (int i=1; i<=6; i++){
+
+            printf("%d\n", i);
+
+        }
+
+        return 0;
+        
+    }
